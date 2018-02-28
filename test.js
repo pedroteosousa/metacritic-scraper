cf350eb4cb5edce4f38f6599a6110414be784165
pedroteosousa <pedroteosousa@gmail.com>
2018-02-28 15:23:53 -0300

    organized files a bit

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 var metacritic = require('./index')
 
-metacritic.search('blade runner', {category: 'movie'}).on('end', (results) => {
+metacritic.search('shape water', {category: 'all'}).on('end', (results) => {
 	metacritic.getMovie(results[0]).on('end', (reviews) => {
+		console.log(reviews)
+	})
+	metacritic.getReviews(results[0]).on('end', (reviews) => {
 		console.log(reviews)
 	})
 }).on('error', (error) => {
